3b1e84ac0457cf9fad6837c40d4e572977c93a48
Samrat Alam <samratalamshanto710@gmail.com>
2025-04-17 20:52:53 +0600

    redis on going

--- main.go
+++ main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"Parking_Slot_in_GO/pkg/config"
 	"Parking_Slot_in_GO/pkg/database"
+	"Parking_Slot_in_GO/pkg/redis"
 	"fmt"
 )
 
 func main() {
 	config.LoadConfig()
 	database.ConnectDb()
+	redis.ConnectRedis()
 	fmt.Println("Application running...")
 }
--- pkg/redis/redis.go
+++ pkg/redis/redis.go
@@ -1,1 +1,27 @@
 package redis
+
+import (
+	"context"
+	"github.com/go-redis/redis/v8"
+	"os"
+)
+
+var (
+	ctx         = context.Background()
+	redisClient *redis.Client
+)
+
+func ConnectRedis() {
+
+	var REDIS_HOST = os.Getenv("REDIS_HOST")
+	var REDIS_PORT = os.Getenv("REDIS_PORT")
+	var REDIS_USER = os.Getenv("REDIS_USER")
+	var REDIS_PASS = os.Getenv("REDIS_PASS")
+
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:     REDIS_HOST + ":" + REDIS_PORT,
+		Password: REDIS_PASS,
+		Username: REDIS_USER,
+		DB:       0, //Connect to default Redis DB (out of 0–15)
+	})
+}
